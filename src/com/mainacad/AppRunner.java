b6138e170cc643912fa1c2de9a8694db4385c627
Alex <ignatenko2207@gmail.com>
2020-03-02 21:31:39 +0200

    Add new code

--- src/com/mainacad/AppRunner.java
+++ src/com/mainacad/AppRunner.java
@@ -3,24 +3,40 @@ package com.mainacad;
 import com.mainacad.model.User;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppRunner {
+
     public static void main(String[] args) {
 
         User user = new User(1, "login123456", "pass", "fname", "lname");
         User user2 = new User(1, "login123", "pass", "fname", "lname");
 
 
-//        Field[] fields = user.getClass().getDeclaredFields();
-//        for (Field field : fields) {
-//            System.out.println(field.getName());
-//        }
-//
-//        Class superUserClass = user.getClass().getSuperclass();
-//        Field[] sFields = superUserClass.getDeclaredFields();
-//        for (Field field : sFields) {
-//            System.out.println(field.getName());
-//        }
+        Field[] fields = user.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(field.getName());
+        }
+
+        Class superUserClass = user.getClass().getSuperclass();
+        Field[] sFields = superUserClass.getDeclaredFields();
+        for (Field field : sFields) {
+            System.out.println(field.getName());
+        }
+
+        User user3 = user.clone();
+
+
+        final String sql = "SELECT ...";
+
+        String sql2 = "INSERT ...";
+        sql2 += " SOMETHING";
+
+        final List<Integer> ids = new ArrayList<>();
+
+
+        List<Integer> ids2 = new ArrayList<>();
 
         try {
             Class<User> type = User.class;
@@ -29,6 +45,7 @@ public class AppRunner {
             try {
                 System.out.println(field.get(user));
                 System.out.println(field.get(user2));
+                System.out.println(field.get(user3));
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
@@ -37,5 +54,23 @@ public class AppRunner {
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
+
+        if (user.getClass().getName().equals("com.mainacad.model.User")) {
+            System.out.println("true");
+        }
+
+        if (user.getClass().getSimpleName().equals("User")) {
+            System.out.println("true");
+        }
+
+        if (user.getClass().equals(User.class)) {
+            System.out.println("true");
+        }
+
+        if (user instanceof User) {
+            System.out.println("true");
+        }
+
+
     }
 }
--- src/com/mainacad/model/User.java
+++ src/com/mainacad/model/User.java
@@ -1,6 +1,11 @@
 package com.mainacad.model;
 
-public class User extends BaseEntity{
+/**
+ * This is User class
+ *
+ */
+
+public class User extends BaseEntity implements Cloneable{
     private String login;
     private String password;
     private String firstName;
@@ -9,6 +14,14 @@ public class User extends BaseEntity{
     public User() {
     }
 
+    /**
+     *
+     * @param id is set in super class
+     * @param login
+     * @param password
+     * @param firstName
+     * @param secondName
+     */
     public User(Integer id, String login, String password, String firstName, String secondName) {
         this.setId(id);
         this.login = login;
@@ -17,4 +30,16 @@ public class User extends BaseEntity{
         this.secondName = secondName;
     }
 
+    /**
+     *
+     * @return clone of {@code com.mainacad.model.User} inctance
+     */
+    public User clone(){
+        try {
+            return (User) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
